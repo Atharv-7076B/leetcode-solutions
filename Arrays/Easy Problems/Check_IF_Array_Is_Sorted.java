4b479590402c0925bad608490bb2728d5ff6fb8f
Atharv Mohite <notam4040@gmail.com>
2026-06-29 17:16:31 +0530

    Check is array is sorted solution

---
+++ Arrays/Easy Problems/Check_IF_Array_Is_Sorted.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isSorted(ArrayList<Integer> nums) {
+        //your code goes here
+        for(int i=1;i<nums.size();i++){
+            if(nums.get(i)<nums.get(i-1)) return false;
+        }return true;
+    }
+}
