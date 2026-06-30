0c8d4ed6141be88ec6a768734c8bad63c788ab31
Atharv Mohite <notam4040@gmail.com>
2026-06-30 21:42:30 +0530

    1581 querry

---
+++ SQL Que/1581_Customer_Who_Visited_But_Did_Not_Make_Any_Transactions.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT customer_id,COUNT(*) as count_no_trans from visits v LEFT JOIN Transactions t ON v.visit_id=t.visit_id WHERE transaction_id is NULL GROUP BY customer_id;
