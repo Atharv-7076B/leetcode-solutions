e425354f752907a851db92f172d36e642349c4f0
Atharv Mohite <notam4040@gmail.com>
2026-07-16 09:34:31 +0530

    Level order traversal

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
 
     static class Node {
@@ -5,62 +8,117 @@ public class BinaryTree {
         Node left;
         Node right;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.left = null;
-            this.right = null; 
+            this.right = null;
         }
-        
+
     }
 
     static class BinaryTreee {
         static int idx = -1;
-        public static Node buildTree(int [] nodes){
+
+        public static Node buildTree(int[] nodes) {
             idx++;
-            if(nodes[idx] == -1) return null;
+            if (nodes[idx] == -1)
+                return null;
 
             Node newNode = new Node(nodes[idx]);
-            newNode.left = new Node(nodes[idx]);
-            newNode.right = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
             return newNode;
+        }
+
+        public static void preOrder(Node root) {
+            System.out.println(root.data);
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public static void inOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public static void postOrder(Node root) {
+            if (root == null)
+                return;
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+        public static void levelOrder(Node root) {
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if(currNode.left!=null){
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right!=null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
         }
     }
 
-    
     public static void main(String[] args) {
         int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTreee bt = new BinaryTreee();
-        Node root = bt.buildTree(nodes);System.out.println(root.data);
+        Node root = bt.buildTree(nodes);
+        // bt.preOrder(root);
+
+        // bt.postOrder(root);
+
+        bt.levelOrder(root);
     }
 }
 
 // static class Node {
-    //     int data;
-    //     Node left;
-    //     Node right;
-    
-    //     Node(int data) {
-        //         this.data = data;
-        //         this.left = null;
-        //         this.right = null;
-        //     }
-        // }
-        
-        // static class BinaryTreee {
-            //     static int idx = -1;
-            
-            //     public static Node buildTree(int nodes[]) {
-                //         idx++;
-                //         if (nodes[idx] == -1) {
-                    //             return null;
-                    //         }
-                    //         Node newNode = new Node(nodes[idx]);
-                    //         newNode.left = buildTree(nodes);
-                    //         newNode.right = buildTree(nodes);
-                    
-                    //         return newNode;
-                    //     }
-                    // }
-                    // BinaryTreee bt = new BinaryTreee();
-                    // Node root = bt.buildTree(nodes);
-                    // System.out.println(root.data);
+// int data;
+// Node left;
+// Node right;
+
+// Node(int data) {
+// this.data = data;
+// this.left = null;
+// this.right = null;
+// }
+// }
+
+// static class BinaryTreee {
+// static int idx = -1;
+
+// public static Node buildTree(int nodes[]) {
+// idx++;
+// if (nodes[idx] == -1) {
+// return null;
+// }
+// Node newNode = new Node(nodes[idx]);
+// newNode.left = buildTree(nodes);
+// newNode.right = buildTree(nodes);
+
+// return newNode;
+// }
+// }
+// BinaryTreee bt = new BinaryTreee();
+// Node root = bt.buildTree(nodes);
+// System.out.println(root.data);
