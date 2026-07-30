26c044f0de0fcbce18b1047014ba7029076675cd
Atharv__Mohite <notam4040@gmail.com>
2026-07-30 18:04:16 +0530

    Time: 3 ms (95.31%), Space: 44.8 MB (78.5%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> l = new ArrayList<>();
+
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                l.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        int[] result = new int[l.size()];
+        int i = 0;
+        for (int num : l) {
+            result[i++] = num;
+        }
+        return result;
+    }
+}
