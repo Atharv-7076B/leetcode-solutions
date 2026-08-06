053d10c3e4a4c9cb2f2bcd5bfd05c79d8c6838d9
Atharv__Mohite <notam4040@gmail.com>
2026-08-06 19:18:44 +0530

    Time: 5 ms (69.28%), Space: 46.3 MB (9.65%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        char[] m = moves.toCharArray();
+        int up = 0;
+        int down = 0;
+        int left = 0;
+        int right = 0;
+
+        for (char ch : m) {
+            if (ch == 'U')
+                up++;
+            if (ch == 'D')
+                down++;
+            if (ch == 'R')
+                right++;
+            if (ch == 'L')
+                left++;
+        }
+        return (left == right && up == down) ? true : false;
+    }
+}
