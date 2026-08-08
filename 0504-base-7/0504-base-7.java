ffe1271a6942722d3eff45b9617a1a8ce6e22644
Atharv__Mohite <notam4040@gmail.com>
2026-08-08 07:56:08 +0530

    Time: 1 ms (70.6%), Space: 42.7 MB (75.85%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,5 +1,20 @@
 class Solution {
     public String convertToBase7(int num) {
-        return Integer.toString(num, 7);
+        if (num == 0)
+            return "0";
+
+        boolean is_negative = num < 0;
+        int n = Math.abs(num);
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int remain = n % 7;
+            sb.append(remain);
+            n /= 7;
+        }
+        if (is_negative)
+            sb.append("-");
+
+        return sb.reverse().toString();
     }
 }
+// return Integer.toString(num, 7);
